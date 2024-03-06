55e113573b1881b858f5f89e201c26f68dc196f6
harbouli <mohamed.harbouli.hb@gmail.com>
2024-03-06 21:59:44 +0100

    feat: Learning how to Generate CRUD Golang code from SQL

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,3 +1,5 @@
+
+
 -- name: CreateAccount :one
 INSERT INTO account (
   owner,
@@ -7,3 +9,22 @@ INSERT INTO account (
   $1, $2, $3 
 )
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM account
+ORDER BY id;
+
+
+-- name: UpdateAccount :one
+UPDATE account
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM account
+WHERE id = $1;
